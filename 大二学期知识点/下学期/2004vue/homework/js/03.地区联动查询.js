c747c23bc94ddd9a391ca0ed8a35c2371e8a9ac3
gaopeng_glb <3011248542@qq.com>
2022-03-13 19:10:26 +0800

    地区联动/部门员工

---
+++ 大二学期知识点/下学期/2004vue/homework/js/03.地区联动查询.js
@@ -0,0 +1,63 @@
+Vue.config.devtools = true
+const vm = new Vue({
+    el: '#app',
+    data() {
+        return {
+            pid: 0,
+            aid: 0,
+            province: [], //省  1
+            paid: 0,
+            city: [], //市  0
+            caid: 0,
+            district: [], //区/县 3
+            daid: 0,
+            street: [], //街道 4
+        }
+    },
+    methods: {
+        queryProvince() {
+            ajax.send('/linkinfo/queryAreaByPid', { pid: this.pid }, (data) => {
+                this.province = data.list
+                this.aiid = this.province[0].aiid
+                this.pid = this.province[0].aid
+                this.paid = this.province[0].aid
+                this.queryCity()
+            })
+        },
+        queryCity() {
+            console.log(this.aid, "---");
+            // console.log(this.pid);
+            ajax.send('/linkinfo/queryAreaByPid', { pid: this.paid }, (data) => {
+                this.city = data.list
+                this.pid = this.city[0].aid
+                this.caid = this.city[0].aid
+                console.log(this.caid);
+                this.queryDistrict()
+            })
+        },
+        queryDistrict() {
+            // console.log(this.pid);
+            ajax.send('/linkinfo/queryAreaByPid', { pid: this.caid }, (data) => {
+                this.district = data.list
+                this.pid = this.district[0].aid
+                this.daid = this.district[0].aid
+                this.queryStreet()
+            })
+        },
+        queryStreet() {
+            // console.log(this.pid);
+            ajax.send('/linkinfo/queryAreaByPid', { pid: this.daid }, (data) => {
+                this.street = data.list
+                this.pid = this.street[0].aid
+                    // console.log(this.pid);
+            })
+        }
+
+
+    },
+    created() {
+        this.queryProvince()
+    },
+})
+
+//pid=上一级aid
