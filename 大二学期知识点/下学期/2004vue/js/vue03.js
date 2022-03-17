eb5543e479ffb4600ed5e557ae573b3912d7298e
gaopeng_glb <3011248542@qq.com>
2022-03-17 21:22:35 +0800

    computed计算属性

--- 大二学期知识点/下学期/2004vue/js/vue03.js
+++ 大二学期知识点/下学期/2004vue/js/vue03.js
@@ -9,7 +9,7 @@ new Vue({
             pid: -1,
             //城市的数据
             clist: [],
-            cid: -1,
+            cid: -1
         }
     },
     methods: {
@@ -43,8 +43,25 @@ new Vue({
     },
     //computed表示计算属性，里面定义的方法都必须要有返回值效果和data一样,可以直接在页面显示
     computed: {
+        city() {
+            for (let i = 0; i < this.clist.length; i++) {
+                if (this.cid == this.clist[i].cid) {
+                    return this.clist[i]
+                }
+                return {}
+            }
+        },
         province() {
-            return "省份变化:" + this.pid
+            //plist里面是全部的省份信息
+            //通过pid可以获取选中的省份编号
+            let province = {}
+            for (let i = 0; i < this.plist.length; i++) {
+                //搜索和pid匹配数据，就是选中完整的省份信息
+                if (this.pid == this.plist[i].pid) {
+                    return this.plist[i]
+                }
+            }
+            return province
         }
     },
     created() {
@@ -52,3 +69,9 @@ new Vue({
         this.queryProvince()
     },
 })
+
+//el：是页面元素id选择器
+//data(){} 这个是页面数据
+//methods:function定义
+//created(){}生命周期钩子函数，vue创建时自动调用
+//computed:计算属性定义函数
