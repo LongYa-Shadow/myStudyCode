c6de217339d62747cf3d22304078af1c737d3a4e
gaopeng_glb <3011248542@qq.com>
2022-04-22 13:16:47 +0800

    文件管理1.0.1

--- 大二学期知识点/下学期/2004vue/homework/fileManage/fileManage.js
+++ 大二学期知识点/下学期/2004vue/homework/fileManage/fileManage.js
@@ -10,22 +10,19 @@ new Vue({
         pageSize: '5'
       },
       list: [], //文件列表
-      fileGroup: [
-        { id: 1, type: 'image' },
-        { id: 2, type: 'text' }
-      ],
-
+      fileGroup: tools.FILE_TYPE,
       queryInfo: {//查询信息
         filename: '',
         fileinfo: '',
         contentType: ''
-      }, 
-      
+      },
+
       dialogVisibleFile: false,//上传模态框
       selectedFile: null,//上传文件
       fileInfo: '',//上传文件信息
       imgdata: '',//上传文件预览
       dialogVisibleImg: false,//图片预览模态框
+      fit: 'cover',
       previewImgUrl: '',//图片预览地址
       loadingTable: false,//表格加载
       loadingUpFile: false,//文件上传加载
@@ -49,7 +46,6 @@ new Vue({
     browserFile() {
       tools.openFile((file) => {
         this.selectedFile = file
-
         // console.log(file);
         /*   if (file.type.substr(0, 6) == 'image/') {
             tools.loadImageDate(file, (data) => {
@@ -61,7 +57,8 @@ new Vue({
       })
     },
     uploadFile() {
-      if (!this.selectedFile || !this.fileInfo) return
+      console.log(this);
+      if (!this.selectedFile || !this.fileInfo) return;
       this.loadingUpFile = true
       ajax.upload(this.selectedFile, { fileinfo: this.fileInfo }, (data) => {
         alert(data.message)
@@ -95,13 +92,12 @@ new Vue({
       this.query()
     },
     previewImg(file) {
-      console.log();
       this.previewImgUrl = ajax.getDownloadUrl(file.fid)
     },
-    //分页
-    pagination() {
-
+    isImg(info) {
+      return info.contentType.substr(0, 6) == 'image/'
     }
+
   },
   created() {
     this.query()
